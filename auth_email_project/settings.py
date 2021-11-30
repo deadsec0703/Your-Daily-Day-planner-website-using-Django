8bccc00aed4f9a6e1f5509ccbf672e1605f90327
Soham <46812992+sohamsv@users.noreply.github.com>
2021-11-30 22:04:16 +0530

    Update settings.py

--- auth_email_project/settings.py
+++ auth_email_project/settings.py
@@ -123,12 +123,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = "/var/www/soham11.pythonanywhere.com/static/"
+STATIC_ROOT = "/var/www/example.com/static/"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = ""
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "batofaneyee@gmail.com"
-EMAIL_HOST_PASSWORD = "csgo4lyf"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
